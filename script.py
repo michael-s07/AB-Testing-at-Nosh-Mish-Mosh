3289532963b8e9172e4b93447a7cac99ead86750
michael-s07 <michael.sosu@amalitech.org>
2021-12-20 17:42:25 +0000

    Initial commit

---
+++ script.py
@@ -0,0 +1,26 @@
+import noshmishmosh
+import numpy as np
+
+all_visitors = noshmishmosh.customer_visits
+paying_visitors = noshmishmosh.purchasing_customers
+
+total_visitor_count = len(all_visitors)
+paying_visitor_count = len(paying_visitors)
+
+baseline_percent = paying_visitor_count / total_visitor_count * 100
+
+print("Baseline percent:",baseline_percent)
+
+payment_history = noshmishmosh.money_spent
+
+average_payment = np.mean(payment_history)
+new_customers_needed = np.ceil(1240 / average_payment)
+
+percentage_point_increase = new_customers_needed / total_visitor_count * 100
+print("Percentage point increase:",percentage_point_increase)
+
+mde = percentage_point_increase / baseline_percent * 100
+
+print("Minimum Detectable Effect:",mde)
+
+ab_sample_size = 	490
